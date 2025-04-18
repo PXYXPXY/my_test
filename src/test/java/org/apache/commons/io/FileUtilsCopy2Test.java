6c653bf31f5ccd5254938f0b69b4c31a40e9e280
PXYXPXY <2472007664@qq.com>
2025-04-18 22:37:34 +0800

    冒烟测试

--- src/test/java/org/apache/commons/io/FileUtilsCopy2Test.java
+++ src/test/java/org/apache/commons/io/FileUtilsCopy2Test.java
@@ -2,6 +2,7 @@ package org.apache.commons.io;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
@@ -16,6 +17,12 @@ public class FileUtilsCopy2Test {
     @BeforeEach
     public void setUp() throws IOException {
         // 初始化测试数据
+    }
+
+    @Test
+    @Tag("smoke")
+    public void testSearch() {
+        // 测试单一功能
     }
 
     @Test
